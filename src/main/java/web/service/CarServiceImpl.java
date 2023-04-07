5d2b19b79c285188469bc41d2da6b09e93a58923
denis <denis@gmail.com>
2023-04-07 07:24:00 +0300

    Change parsing logic: now parsing runs one time.

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -6,20 +6,27 @@ import web.model.Cars;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    public List<Car> getCars(Integer quantity) throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(Cars.class);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Cars cars = (Cars) unmarshaller.unmarshal(
-                getClass().getClassLoader().getResourceAsStream("carList.xml"));
-        List<Car> carList = cars.getCarList();
-        return quantity == null ? carList : carList.stream().limit(quantity).collect(Collectors.toList());
+    private static List<Car> carList;
+    {
+        Cars cars;
+        try {
+            cars = (Cars) JAXBContext.newInstance(Cars.class).createUnmarshaller().unmarshal(
+                    getClass().getClassLoader().getResourceAsStream("carList.xml"));
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+        carList = cars.getCarList();
+
+        System.out.println("parsing xml");
     }
 
+    public List<Car> getCars(Integer quantity) {
+        return quantity == null ? carList : carList.stream().limit(quantity).collect(Collectors.toList());
+    }
 }
