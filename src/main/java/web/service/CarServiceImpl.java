6f3458241dbf7e8514f9e6d9930abc00e0fe1d0d
denis <denis@gmail.com>
2023-04-07 07:43:52 +0300

    Delete parsing

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,16 +3,24 @@ package web.service;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class CarServiceImpl {
-    private List<Car> carList;
+public class CarServiceImpl implements CarService {
 
-
-
+    private static final List<Car> carList;
+    static {
+        carList = new ArrayList<>();
+        carList.add(new Car("Mitsubishi", "Pajero Sport", 2900000));
+        carList.add(new Car("Land Rover", "Discovery", 6000000));
+        carList.add(new Car("УАЗ", "Patriot", 1100000));
+        carList.add(new Car("Renault", "Duster", 1100000));
+        carList.add(new Car("Toyota", "RAV4", 2200000));
+    }
 
     public List<Car> getCars(Integer quantity) {
-        return quantity == null || quantity > carList.size() ? carList : carList.subList(0, quantity);
+        return quantity == null ? carList : carList.stream().limit(quantity).collect(Collectors.toList());
     }
 }
